821afacb4b7abb45a7a4df43b3789377a434ae38
Roman Semykin <KindPanic@mail.ru>
2021-04-22 23:03:21 +0300

    2. Сокобан. Тестовое задание.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,42 +1,37 @@
 package ru.job4j.puzzle;
 
 public class Win {
-    public static boolean monoVertical(int[][] board) {
-        boolean rsl = false;
-        for (int i = 0; i < board[0].length; i++) {
-            if (board[0][i] != 0) {
-                for (int j = 0; j < board.length - 1; j++) {
-                    if (board[j][i] == board[j + 1][i] && board[j][i] != 0) {
-                        rsl = true;
-                    } else {
-                        rsl = false;
-                        break;
-                    }
-                }
+    public static boolean monoVertical(int[][] board, int index) {
+        boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][index] != 1) {
+                rsl = false;
+                break;
             }
         }
         return rsl;
     }
 
-    public static boolean monoHorizontal(int[][] board) {
-        boolean rsl = false;
+    public static boolean monoHorizontal(int[][] board, int index) {
+        boolean rsl = true;
         for (int i = 0; i < board.length; i++) {
-            if (board[i][0] != 0) {
-                for (int j = 0; j < board[i].length - 1; j++) {
-                    if (board[i][j] == board[i][j + 1] && board[i][j] != 0) {
-                        rsl = true;
-                    } else {
-                        rsl = false;
-                        break;
-                    }
-                }
-        }
+            if (board[index][i] != 1) {
+                rsl = false;
+                break;
+            }
         }
         return rsl;
     }
 
-        public static boolean check ( int[][] board){
-            return monoHorizontal(board) || monoVertical(board);
+        public static boolean check (int[][] board){
+            boolean rsl = false;
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                    rsl = true;
+                    break;
+                }
+            }
+            return rsl;
         }
     }
 
